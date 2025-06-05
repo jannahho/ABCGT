1f3e514277ac1651a099905e966ac8d5ce8524d6
Hannah Jo <112975722+jannahho@users.noreply.github.com>
2025-06-04 20:06:48 -0700

    9

--- script.js
+++ script.js
@@ -90,5 +90,101 @@ document.addEventListener("DOMContentLoaded", function() {
       clickable: true,
     },
   });
+});
 
+
+
+// Mslider below:
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  let jsonDatabase = [{
+      "image": "cardtest1.png",
+      "backgroundcolor": "#F6EF6E"
+    },
+    {
+      "image": "cardtest2.png",
+      "backgroundcolor": "#F9C5C4"
+    },
+    {
+      "image": "cardtest3.png",
+      "backgroundcolor": "#91CD97"
+    },
+    {
+      "image": "cardtest4.png",
+      "backgroundcolor": "#D85228"
+    },
+    {
+      "image": "cardtest5.png",
+
+      "backgroundcolor": "#9FDAF7"
+    },
+    {
+      "image": "cardtest6.png",
+
+      "backgroundcolor": "#D85228"
+  },
+    {
+      "image": "cardtest7.png",
+
+      "backgroundcolor": "#F9C5C4"
+    }
+
+  ];
+
+
+  for (let i = 0; i < jsonDatabase.length; i++) {
+    createElement(jsonDatabase[i]);
+  }
+
+  function createElement(incomingJSON) {
+    // let newListElement = document.createElement('li');
+    // newListElement.classList.add("glide__slide");
+
+    let newContentElement = document.createElement("DIV");
+    newContentElement.style.backgroundColor = incomingJSON['backgroundcolor'];
+    newContentElement.classList.add('McontentDiv');
+    newContentElement.classList.add('swiper-slide');
+
+
+    let newProjectImage = document.createElement("IMG");
+    newProjectImage.classList.add("MprojectImage");
+    newProjectImage.src = incomingJSON['image'];
+    newContentElement.appendChild(newProjectImage);
+
+
+    // newListElement.appendChild(newContentElement);
+    document.getElementById('myMslides').appendChild(newContentElement);
+
+  }
+
+
+  var swiper = new Swiper(".Mswiper", {
+    effect: "coverflow",
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: true,
+    },
+    coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 });
